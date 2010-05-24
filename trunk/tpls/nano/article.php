a5a7496fba489fe85ec2e494631a6f7c9fea2a99
eugene.dsda <eugene.dsda@01a6e054-96f9-c348-fed7-822573d5eef6>
2010-05-24 22:07:27 +0000

    disqus.com comments added

--- trunk/tpls/nano/article.php
+++ trunk/tpls/nano/article.php
@@ -7,4 +7,19 @@
 				<?=$_article['full'];?>
 				<?=$_article['time'];?> by <?=$_cfg['author'];?>
 			</DIV>
+			
+				<div id="disqus_thread"></div>
+				<script type="text/javascript">
+				  /**
+					* var disqus_identifier; [Optional but recommended: Define a unique identifier (e.g. post id or slug) for this thread] 
+					*/
+				  (function() {
+				   var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+				   dsq.src = 'http://5ka.disqus.com/embed.js';
+				   (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+				  })();
+				</script>
+				<noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript=5ka">comments powered by Disqus.</a></noscript>
+				<a href="http://disqus.com" class="dsq-brlink">blog comments powered by <span class="logo-disqus">Disqus</span></a>
+							
 		</DL>
